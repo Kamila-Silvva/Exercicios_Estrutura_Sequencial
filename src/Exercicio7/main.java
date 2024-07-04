5901bc0bf97a04655adf391c8e8894fe7afe937f
kamilaalvesdemiranda@gmail.com <kamilaalvesdemiranda@gmail.com>
2024-07-04 16:53:55 -0300

    Exercicios

---
+++ src/Exercicio7/main.java
@@ -0,0 +1,21 @@
+package Exercicio7;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+
+        System.out.println("Digite o valor: ");
+        numero = scanner.nextInt();
+
+        if (numero >= 0) {
+            System.out.println("Positivo");
+        }
+        else {
+            System.out.println("Negativo");
+        }
+    }
+}
