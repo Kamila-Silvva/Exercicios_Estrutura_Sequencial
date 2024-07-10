80bd0afa64764224bbc990de4a1b078da2e19742
Kamila-Silva <kamilaalvesdemiranda@gmail.com>
2024-07-10 16:29:03 -0300

    Aula 23 seção 3

---
+++ src/Exercicio11/Main.java
@@ -0,0 +1,12 @@
+package Exercicio11;
+
+public class Main {
+    public static void main(String[] args) {
+        double fahrenheit = 155;
+        double celcius;
+
+        celcius = (fahrenheit - 32) * 5.0/9.0;
+
+        System.out.println("Celcius: " + celcius);
+    }
+}
